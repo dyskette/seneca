23073986616f9e3d469d5f3c0c1d736094d48d4a
Eddy Castillo <dyskette@gmail.com>
2017-08-08 08:21:43 -0500

    Changes for new webview

--- softbook/window.py
+++ softbook/window.py
@@ -1,6 +1,6 @@
 # window.py
 #
-# Copyright (C) 2017 Dylan Smith
+# Copyright (C) 2017 Eddy Castillo
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -47,12 +47,11 @@ class ApplicationWindow(Gtk.ApplicationWindow):
         Gtk.ApplicationWindow.__init__(self, application=application)
         self.init_template()
         self.book = None
-        self.settings = {}
 
-        variant = GLib.Variant.new_string('black')
-        action = Gio.SimpleAction.new_stateful('color', variant.get_type(), variant)
-        action.connect('change-state', self.change_color)
-        self.add_action(action)
+        variant = GLib.Variant('s', 'light')
+        self.color_action = Gio.SimpleAction.new_stateful('color', variant.get_type(), variant)
+        self.color_action.connect('change-state', self.on_change_color)
+        self.add_action(self.color_action)
 
         # Initialize with headerbar buttons disabled
         self.prev_btn.set_sensitive(False)
@@ -62,108 +61,84 @@ class ApplicationWindow(Gtk.ApplicationWindow):
     def open_file(self, _gfile):
         try:
             self.book = Book(_gfile)
-            # TODO: Settings probably should be in book.py
-            # TODO: Connect settings to page load (fami, weig, styl, stre,)
-            # FIXME: Settings only last current page. See previous TODO
-            # self.settings = Settings.load()
-            self.settings = {'font-family': 'Roboto Condensed',
-                             'font-weight': '400',
-                             'font-style': 'normal',
-                             'font-stretch': 'condensed',
-                             'font-size':  17,
-                             'font-lineheight': 1.8,
-                             'color': 'light'}
-            self.change_font(self.settings['font-family'],
-                             self.settings['font-weight'],
-                             self.settings['font-style'],
-                             self.settings['font-stretch'],
-                             self.settings['font-size'])
-            self.change_lineheight(self.settings['font-lineheight'])
+            # Add to view
+            self.main_view.pack_start(self.book, True, True, 0)
+            self.book.show_all()
 
-            # Add to view and show it
-            self.main_view.pack_end(self.book.docview, True, True, 0)
-            self.book.docview.show_all()
             # Make headerbar buttons available
             self.prev_btn.set_sensitive(True)
             self.next_btn.set_sensitive(True)
             self.open_menu.set_sensitive(True)
+
             # Sync buttons with settings
-            pango_font = pangoFont(self.settings['font-family'],
-                                   self.settings['font-weight'],
-                                   self.settings['font-style'],
-                                   self.settings['font-stretch'],
-                                   self.settings['font-size'])
+            _font = self.book.get_font()
+            pango_font = pangoFont(_font[0], _font[1], _font[2], _font[3], _font[4])
             self.font_btn.set_font_desc(pango_font.desc)
-            self.font_label.set_label('{0}px'.format(self.book.fontsize))
-            self.lineheight_label.set_label('{0}pt'.format(self.book.lineheight))
+
+            self.font_label.set_label('{0}px'.format(self.book.get_fontsize()))
+            self.lineheight_label.set_label('{0}pt'.format(self.book.get_lineheight()))
+
+            variant = GLib.Variant('s', self.book.get_color())
+            self.color_action.set_state(variant)
         except Exception as e:
-            print(e)
+            raise ValueError(e)
             #TODO: Use an application notification.
 
     def change_fontsize(self, fontsize):
-        self.book.fontsize = fontsize
+        self.book.set_fontsize(fontsize)
         self.font_less.set_sensitive(fontsize > 8)
         self.font_more.set_sensitive(fontsize < 32)
-        self.font_label.set_label('{0}px'.format(self.book.fontsize))
-
-    def change_font(self, fontfamily, fontweight, fontstyle, fontstretch, fontsize):
-        self.book.fontfamily = fontfamily
-        self.book.fontweight = fontweight
-        self.book.fontstyle = fontstyle
-        self.book.fontstretch = fontstretch
-        self.change_fontsize(fontsize)
+        self.font_label.set_label('{0}px'.format(self.book.get_fontsize()))
 
     def change_lineheight(self, lineheight):
-        self.book.lineheight = float(format(lineheight, '1.2f'))
+        self.book.set_lineheight(float(format(lineheight, '1.2f')))
         self.lineheight_less.set_sensitive(lineheight > 1.0)
-        self.lineheight_more.set_sensitive(lineheight < 2.4)
-        self.lineheight_label.set_label('{0}pt'.format(self.book.lineheight))
+        self.lineheight_more.set_sensitive(lineheight < 2.8)
+        self.lineheight_label.set_label('{0}pt'.format(self.book.get_lineheight()))
 
-    def change_color(self, action, value):
-        print(value.get_string())
+    def on_change_color(self, action, value):
         action.set_state(value)
-        # self.book.color = _color
-        # if _color == 'white':
+        self.book.set_color(value.get_string())
 
     @GtkTemplate.Callback
     def on_font_set(self, widget):
         pango_fontdesc = widget.get_property('font-desc')
         css_font = cssFont(pango_fontdesc)
 
-        self.change_font(css_font.family,
-                         css_font.weight,
-                         css_font.style,
-                         css_font.stretch,
-                         css_font.size)
+        self.book.set_font([css_font.family,
+                            css_font.weight,
+                            css_font.style,
+                            css_font.stretch,
+                            css_font.size])
 
     @GtkTemplate.Callback
     def on_prev_btn(self, widget):
-        self.book.docview.page_prev()
+        self.book.page_prev()
 
     @GtkTemplate.Callback
     def on_next_btn(self, widget):
-        self.book.docview.page_next()
+        self.book.page_next()
 
     @GtkTemplate.Callback
     def on_font_less(self, widget):
-        self.change_fontsize(self.book.fontsize - 1)
+        self.change_fontsize(self.book.get_fontsize() - 1)
 
     @GtkTemplate.Callback
     def on_font_default(self, widget):
-        self.change_fontsize(self.book.fontsize_default)
+        self.change_fontsize(20)
 
     @GtkTemplate.Callback
     def on_font_more(self, widget):
-        self.change_fontsize(self.book.fontsize + 1)
+        self.change_fontsize(self.book.get_fontsize() + 1)
 
     @GtkTemplate.Callback
     def on_lineheight_less(self, widget):
-        self.change_lineheight(self.book.lineheight - 0.2)
+        self.change_lineheight(self.book.get_lineheight() - 0.2)
 
     @GtkTemplate.Callback
     def on_lineheight_default(self, widget):
-        self.change_lineheight(self.book.lineheight_default)
+        self.change_lineheight(1.6)
 
     @GtkTemplate.Callback
     def on_lineheight_more(self, widget):
-        self.change_lineheight(self.book.lineheight + 0.2)
+        self.change_lineheight(self.book.get_lineheight() + 0.2)
